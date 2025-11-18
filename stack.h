f38cae2ddb2701d5fe3cedca290e3c333510f8f0
Auromera <Autoboyrunner@gmail.com>
2025-11-18 04:19:50 +0000

    upd

--- stack.h
+++ stack.h
@@ -17,7 +17,14 @@ void Stack::push(int x){
   NodePtr new_node=new NODE(x);
   if(new_node){
             // Left missing for exercises…
+    if (new_node) {
+        // Insert node at the top of the stack
+        new_node->set_next(top);   // new node points to old top
+        top = new_node;            // update top to new node
+        size++;
+
    }
+}
  
          // Left missing for exercises…
     
@@ -25,20 +32,33 @@ void Stack::push(int x){
 
 int Stack::pop(){
         NodePtr t=top;
+        if(t!=NULL){
         int value;
-        value=t->get_value();
+        value =t ->get_value();
     // Left missing part for exercises
+    top = top ->get_next();
         delete t;
         return value;
 	//be careful of the empty stack!!!
     }
+    cout <<"Empty stack" <<endl;
+    return 0;
+    }
 
-Stack::Stack(){
+
+    Stack::Stack(){
+
+
     //initialize stack
-    
+    top = NULL;
+    size = 0;
 }
 Stack::~Stack(){
     //delete all remaning stack (i.e. pop all) 
+    cout<<"Clear the stack"<<endl;
+       while (top != NULL) {
+        pop();    // pop until empty
+    }
     
 }
 
